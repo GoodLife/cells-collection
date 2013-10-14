651a0f2b820349dbde67ceadba611f4f52d10bb9
lulalala <mark@goodlife.tw>
2013-10-14 10:48:49 +0800

    Caching for render_cell_collection method. This a bit hacky, but it now uses read_multi to fetch items at once.

--- lib/cells-collection.rb
+++ lib/cells-collection.rb
@@ -1,3 +1,4 @@
 require "cells-collection/version"
 require "cells-collection/cells/rails"
 require 'cells-collection/cell/rendering'
+require 'cells-collection/cell/caching'
---
+++ lib/cells-collection/cell/caching.rb
@@ -0,0 +1,63 @@
+require 'active_support/cache'
+
+module Cell
+  module Caching
+    module ClassMethods
+      def render_cell_collection_for(name, state, *args)
+        args_for_single = args.dup
+        collection = args[1]
+
+        return '' if collection.empty?
+
+        item_to_key = {}
+        item_to_cell = {} # Cache initialized cell object
+
+        # Create item-key mapping table for cacheable cells
+        collection.each do |item|
+          args_for_single[1] = item # Prepare args for single item
+          cell = create_cell_for(name, *args_for_single)
+
+          item_to_cell[item] = cell
+
+          if cell.cache?(state, *args)
+            # Remove first arg, because compute_key does not want the cell object as first argument.
+            # I don't know where is this stripped in single cell rendering code.
+            item_to_key[item] = cell.compute_key(state, *(args_for_single[1..-1]))
+          end
+        end
+
+        # cache.read_multi & cache.write interfaces may require mutable keys, ie. dalli 2.6.0
+        mutable_keys = item_to_key.values.map(&:dup)
+
+        if options = cache_options[state]
+          cached = cache_store.read_multi(*mutable_keys, options)
+        else
+          cached = cache_store.read_multi(*mutable_keys)
+        end
+
+        rendered = ''
+
+        collection.each do |item|
+          args_for_single[1] = item # Prepare args for single item
+
+          cell = item_to_cell[item]
+
+          # Get key of item, and check if cached has that key
+          if cell.cache?(state, *args) && cached_result = cached[item_to_key[item]]
+            rendered << cached_result
+          else
+            yield cell if block_given?
+
+            rendered << render_cell_state(cell, state, *args_for_single)
+          end
+        end
+
+        return rendered.html_safe
+      end
+    end
+
+    def compute_key(state, *args)
+      self.class.state_cache_key(state, call_state_versioner(state, *args))
+    end
+  end
+end
